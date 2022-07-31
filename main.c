4d9947cd890919d2d8d48acc595db29eaa1e167e
bhagat2296 <103653090+bhagat2296@users.noreply.github.com>
2022-07-31 22:32:57 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,370 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*int n;
+    printf("enter how many times you want to see on the screen\n");                 //question 1 by for loop
+    scanf("%d",&n);
+    for(int i=0;i<=n;i++)
+    printf("My SirG\n");*/
+
+
+
+    /*int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+    int i=0;
+    while(i<=n)                                                                      //by while loop
+    {
+        printf("My SirG \n");
+        i++;
+
+    }*/
+
+
+
+   /* int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+    int i=0;
+    do                                                                              //by do while loop
+    {
+        printf("My Sirg \n");
+        i++;
+    }while(i<=n);*/
+
+
+
+
+
+    /*int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)                                                        //Qustion 2 by for loop
+        printf("%d\n",i);*/
+
+
+
+        /*int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+        int i=1;                                                                //By while loop
+        while(i<=n)
+    {
+        printf("%d\n",i);
+        i++;
+    }*/
+
+
+    /*
+    int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+    int i=1;                                                                     //By do while loop
+    do
+    {
+        printf("%d\n",i);
+        i++;
+    }while(i<=n);*/
+
+
+
+
+    /*int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+    for(int i=n;i>=1;i--)                                                        //Qustion 3 by for loop
+        printf("%d\n",i);*/
+
+
+
+      /*  int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+        int i=n;                                                                   //By while loop
+        while(i>=1)
+    {
+        printf("%d\n",i);
+        i--;
+    }*/
+
+
+
+    /*int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+
+    int i=n;                                                                        //By do while loop
+    do
+    {
+        printf("%d\n",i);
+        i--;
+    }while(i>=1);*/
+
+
+
+
+
+    /*int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+
+    for(int i=1;i<=2*n;)                                                               //Qustion 4 by for loop
+        {
+            printf("%d\n",i);
+            i=i+2;
+        }*/
+
+
+
+        /*int n;
+        printf("enter how many times you want to see on the screen\n");
+        scanf("%d",&n);
+
+       int i=1;                                                                      //By while loop
+        while(i<=2*n)
+    {
+        printf("%d\n",i);
+        i+=2;;
+    }*/
+
+
+
+     /*int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+     int i=1;                                                                        //By do while loop
+    do
+    {
+        printf("%d\n",i);
+        i+=2;
+    }while(i<=2*n);*/
+
+
+
+
+
+    /*int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+
+    for(int i=2*n;i>=1;i--)                                                         //Qustion 5 by for loop
+        {
+            if(i%2)
+
+            {
+                printf("%d\n",i);
+            }
+
+        }*/
+
+
+
+       /*int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+    printf("\n\n");
+
+       int i=2*n;                                                                       //By while loop
+        while(i>=1)
+    {
+        if(i%2)
+        {
+            printf("%d\n",i);
+        }
+        i--;
+    }*/
+
+
+
+
+     /*int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+    printf("\n\n");
+     int i=2*n;                                                                      //By do while loop
+    do
+    {
+        if(i%2)
+        {
+        printf("%d\n",i);
+
+        }
+        i--;
+    }while(i>=1);*/
+
+
+
+    /*int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+    printf("\n\n");
+   for(int i=1;i<=n;i++)                                                                //Qustion 6 by for loop
+        {
+          printf("%d\n",2*i);
+
+        }*/
+
+
+
+
+
+   /* int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+    printf("\n\n");
+        int i=1;                                                                         //By while loop
+        while(i<=n)
+    {
+        printf("%d\n",2*i);
+        i++;
+    }*/
+
+
+
+    /* int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+    printf("\n\n");
+
+     int i=1;                                                                         //By do while loop
+    do
+    {
+        printf("%d\n",2*i);
+        i++;
+    }while(i<=n);*/
+
+
+
+
+    /*int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+    printf("\n\n");
+
+      for(int i=n;i>=1;i--)                                                          //Qustion 7 by for loop
+        {
+            printf("%d\n",2*i);
+
+        }*/
+
+
+
+
+        /*int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+    printf("\n\n");
+        int i=n;                                                                  //By while loop
+        while(i>=1)
+    {
+        printf("%d\n",2*i);
+        i--;
+    }*/
+
+
+
+     /*
+
+     int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+    printf("\n\n");
+     int i=n;                                                                  //By do while loop
+    do
+    {
+        printf("%d\n",i*2);
+        i--;
+    }while(i>=1);*/
+
+
+
+
+    /*int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+    printf("\n\n");
+    for(int i=1;i<=n;i++)                                                   //Qustion 8 by for loop
+        printf("%d\n",i*i);*/
+
+
+
+
+    /*int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+    printf("\n\n");
+        int i=1;                                                            //By while loop
+        while(i<=n)
+    {
+        printf("%d\n",i*i);
+        i++;
+    }*/
+
+
+
+
+    /*int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+    printf("\n\n");
+    int i=1;                                                              //By do while loop
+    do
+    {
+        printf("%d\n",i*i);
+        i++;
+    }while(i<=n);*/
+
+
+
+
+
+    /*int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+    printf("\n\n");
+    for(int i=1;i<=n;i++)                                               //Qustion 9 by for loop
+        printf("%d\n",i*i*i);*/
+
+
+
+        /*int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+    printf("\n\n");
+        int i=1;                                                           //By while loop
+        while(i<=n)
+    {
+        printf("%d\n",i*i*i);
+        i++;
+    }*/
+
+
+
+
+    /*int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+    printf("\n\n");
+    int i=1;                                                                    //By do while loop
+    do
+    {
+        printf("%d\n",i*i*i);
+        i++;
+    }while(i<=n);*/
+
+
+
+
+    /*int n;
+    printf("enter how many times you want to see on the screen\n");
+    scanf("%d",&n);
+    printf("\n\n");
+    for(int i=1;i<=10;i++)                                                    //Question 10 By for loop
+        printf("%d\n",n*i);*/
+
+
+    return 0;
+}
+
